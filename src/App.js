c6e7550c05eaee81f48cad80d5cd116546ada193
anish391 <argads@gmail.com>
2020-05-27 08:13:53 +0000

    Finish implementing basic React Todo.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ToDoItem = ({text}) => (
+	<li>{text}</li>
+);
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			toDos: [],
+			newToDo: ''
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	
+	handleSubmit(e) {
+		e.preventDefault();
+		const toDos = [...this.state.toDos, this.state.newToDo];
+		this.setState({toDos, newToDo: ''});
+	}
+	
+	render() {
+		const {newToDo} = this.state;
+		const todos = this.state.toDos.map((todo, index) => (
+			<ToDoItem key={index} text={todo}/>	
+		));
+		
+		return (
+			<div className="App">
+				<h1>Simple Todo App</h1>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						className="todo-input"
+						autoComplete="off"
+						type="text"
+						name="newToDo"
+						placeholder="What needs to be done?"
+						value={newToDo}
+						onChange={(e) => this.setState({[e.target.name]: e.target.value})}
+					/>
+					<button type="submit" className="save-button">SAVE</button>
+				</form>
+				<div className="todo-content">
+					<ol>
+						{todos}
+					</ol>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
